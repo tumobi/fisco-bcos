e5382d334df66d74504b6808d8e5b46f55551379
tumobi <tumobi@163.com>
2021-05-31 15:08:19 +0800

    精简 fisco 官方 sdk

--- test/contracts/v4/BooleanArray.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.23;
-
-contract BooleanArray {
-    string name;
-
-    function noop(bool[2] memory a, bool[2] memory b) public returns (bool) {
-        return b[0];
-    }
-}
